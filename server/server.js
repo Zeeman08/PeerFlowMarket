e91f5705d1cc0352e768a2227a65affb667d6771
GawwyG <2105094@ugrad.cse.buet.ac.bd>
2024-01-05 16:46:46 +0600

    rubai gay

--- server/server.js
+++ server/server.js
@@ -1,10 +1,55 @@
 const express = require("express");
 const app = express();
 
-app.get("/getNiggers", (req, res) => {
-    console.log("get all the niggers");
-})
+app.use(express.json());
 
+//Get all people
+app.get("/getPeople", (req, res) => {
+  console.log("Got all people")
+  res.status(200).json({
+    status: "success",
+    data: {
+      people: ["Gawwy", "Rubaiyat"]
+    }
+  });
+});
+
+//Get a person
+app.get("/getPeople/:id", (req, res) => {
+  console.log("Got a person")
+  res.status(200).json({
+    status: "success",
+    data: {
+      people: "Gawwy"
+    }
+  });
+});
+
+//Create a person
+app.post("/createPeople", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    status: "success"
+  });
+});
+
+//Update a person
+app.put("/updatePeople/:id", (req, res) => {
+  console.log(req.params);
+  console.log(req.body);
+  res.status(200).json({
+    status: "success"
+  });
+});
+
+//Delete a person
+app.delete("/deletePeople/:id", (req, res) => {
+  console.log("Deleting successful");
+  console.log(req.params);
+  res.status(204).json({
+    status: "success"
+  });
+});
 
 const port = 3005;
 app.listen(port, () => {
