b72b4a1bf01de3806f8e818a9700f89a3d0f35ba
Zeeman08 <2105104@ugrad.cse.buet.ac.bd>
2024-01-05 16:51:28 +0600

    f

--- server/server.js
+++ server/server.js
@@ -1,55 +1,5 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
-//middleware
-app.use(express.json());
-app.get("/getPeople", (req, res) => {
-    //console.log("get all the people");
-    res.json({
-        status: "success",
-        data: {
-            people: ["gawwy", "rubaiyat"]
-        }
-    })
-})
-//get a person
-app.get("/getPeople/:id", (req,res) => {
-    console.log(req.params);
-    res.status(201).json({
-        status: "success",
-        data: {
-            person: req.params.id
-        }
-    })
-});
-//create a person
-app.post("/createPeople", (req,res) => {
-    console.log(req.body);
-});
-//update a person
-app.put("/updatePeople/:id", (req,res) => {
-    console.log(req.params.id);
-    console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data: {
-            person: req.params.id
-        }
-    })
-});
-
-//delete a person
-app.delete("/deletePeople/:id", (req,res) => {
-    console.log("delete people");
-    console.log(req.params.id);
-    res.status(201).json({
-        status: "success",
-        data: {
-            person: req.params.id
-        }
-    });
-}   );
-=======
 
 app.use(express.json());
 
@@ -100,7 +50,6 @@ app.delete("/deletePeople/:id", (req, res) => {
     status: "success"
   });
 });
->>>>>>> e91f5705d1cc0352e768a2227a65affb667d6771
 
 console.log("test");
 const port = 3005;
