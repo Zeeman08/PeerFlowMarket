9629b7006bc807370b2a710de6e7103f204db689
GawwyG <2105094@ugrad.cse.buet.ac.bd>
2024-01-07 12:54:49 +0600

    asd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './routes/Home';
+import User from './routes/User';
+import UserUpdate from './routes/UserUpdate';
 
 const App = () => {
   return (
     <div>
         <h1>Gawwy's Version</h1>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/user/:id" element={<User />} />
+                <Route path="/user/:id/update" element={<UserUpdate />} />
+            </Routes>
+        </Router>
     </div>
   )
 }
---
+++ client/src/routes/UserUpdate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserUpdate = () => {
+  return (
+    <div>
+      <h2>UserUpdate</h2>
+    </div>
+  )
+}
+
+export default UserUpdate;
