a415082c5f3634b988932a4e40ddb573aaaed1ae
Zeeman08 <2105104@ugrad.cse.buet.ac.bd>
2024-01-09 18:15:39 +0600

    added user.jsx

--- client/src/routes/User.jsx
+++ client/src/routes/User.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
 
 const User = () => {
+  //Getting id from link
+  const {id} = useParams();
+
+  //Handling form stuff
+  const[name, setName] = useState("");
+
+  //Storing the data from database into user using setUser function
+  const[user, setUser] = useState([]);
+
+  //For going back to home page
+  let navigate = useNavigate();
+
+
+  //The useEffect hook that calls the getUser() function
+  useEffect(() => {
+
+    //The async function that fetches the data from the database
+    const getUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:3005/getPeople/${id}`);
+        const jsonData = await response.json();
+        //console.log(jsonData);
+        setUser(jsonData.data.people);
+      }
+      catch (err) {
+        console.log(err);
+      }
+    };
+
+    //Being called
+    setName(user.name);
+    getUser();
+  }, [id, user.name]);
+
+
+
   return (
-    <div>
-      <h2>User</h2>
-    </div>
+    <Fragment>
+      //display the user id and name
+      <h1 className='text-center mt-5'>User {id} {name}</h1>
+    </Fragment>
   )
 }
 
